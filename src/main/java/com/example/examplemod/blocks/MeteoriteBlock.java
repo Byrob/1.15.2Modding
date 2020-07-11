27bf148339905ae39e47eefa66ed89e9bdf66546
BayBlade <byrobnorris@gmail.com>
2020-07-11 11:00:26 -0400

    Block_problems

--- src/main/java/com/example/examplemod/blocks/MeteoriteBlock.java
+++ src/main/java/com/example/examplemod/blocks/MeteoriteBlock.java
@@ -1,8 +1,17 @@
 package com.example.examplemod.blocks;
 
-public class MeteoriteBlock 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class MeteoriteBlock extends Block
 {
 	
-	
+	public MeteoriteBlock() {
+		super(Block.Properties.create(Material.IRON)
+				.hardnessAndResistance(5.0f, 6.0f)
+				
+				
+				);
+	}
 
 }
